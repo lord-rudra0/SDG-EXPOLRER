e02e011b194d28fb05fb374e5b247ec136a23e57
lord-rudra0 <rudrapratapsingh2026@cs.ajce.in>
2024-10-11 23:55:54 +0530

    1234

--- app.py
+++ app.py
@@ -379,7 +379,5 @@ def handle_question():
     key = data.get('key')
     value = data.get('value')
 
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=False)
+    app.run(debug=False)
