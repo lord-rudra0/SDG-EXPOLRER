ba32bf2223a953788c07554f53ff099528f6fedb
rudra pratap singh <rudrapratapsingh2026@cs.ajce.in>
2024-11-20 03:10:23 +0530

    updare lonchain for deploy

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import InMemoryVectorStore
 from langchain import LLMChain, PromptTemplate
 from pypdf import PdfReader
-from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain
+from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_community.document_loaders import PyPDFLoader
 from flask_cors import CORS
